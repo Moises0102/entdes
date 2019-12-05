fad8449ec570ba1b74513250152af9cc6a691eec
Moises0102 <57746868+Moises0102@users.noreply.github.com>
2019-12-05 10:47:26 +0100

    Add files via upload

---
+++ entdes/src/ejercicios/metodos.java
@@ -0,0 +1,39 @@
+package ejercicios;
+
+public class metodos {
+
+	public static void main(String[] args) {
+		hola();
+		numero();
+	}
+
+	
+	public static void hola() {
+		
+		String a = "ho";
+		String b = "la";
+		
+		String c = a + b;
+		
+		System.out.println(c);
+	}
+	
+	
+	
+	public static void numero() {
+		
+		char a = 67;
+		
+		System.out.println(a);
+		
+	}
+	
+	public static void asci() {
+		
+		char b = 8;
+		
+		double c = (double) b;
+		
+		System.out.println(c);
+	} 
+}
